568be733803e1cd9dd8ddc9b3c767d6786fb15f8
andrealucia98 <47798403+andrealucia98@users.noreply.github.com>
2019-06-18 02:20:01 -0600

    Add files via upload

--- js/my-app.js
+++ js/my-app.js
@@ -30,92 +30,17 @@ var myApp = new Framework7({
     	url: 'radio.html',
     	name: 'radio',
   		},
-		
-		
-		{
-		path: '/perfil/',
-    	url: 'perfil.html',
-    	name: 'perfil',
-  		},
-		
-		{
-		path: '/calendario/',
-    	url: 'calendario.html',
-    	name: 'calendario',
-  		},
-
-		
-		{
-		path: '/form/',
-    	url: 'form.html',
-    	name: 'form',
-  		},
-		
 	
 		{
-		path: '/list/',
-    	url: 'list.html',
-    	name: 'list',
+		path: '/profile/',
+    	url: 'profile.html',
+    	name: 'profile',
   		},
 		
 		
 		
-		{
-		path: '/menu/',
-    	url: 'menu.html',
-    	name: 'menu',
-  		},
 		
-		{
-		path: '/info/',
-    	url: 'info.html',
-    	name: 'info',
-  		},
-		
-		{
-		path: '/adopta/',
-    	url: 'adopta.html',
-    	name: 'adopta',
-  		},
-		
-		
-		
-		{
-		path: '/about/',
-    	url: 'about.html',
-    	name: 'about',
-  		},
-		
-		
-		{
-		path: '/rastrear/',
-    	url: 'rastrear.html',
-    	name: 'rastrear',
-  		},
-		
-		{
-		path: '/lugares/',
-    	url: 'lugares.html',
-    	name: 'lugares',
-  		},
-		
-		{
-		path: '/veterinario/',
-    	url: 'veterinario.html',
-    	name: 'veterinario',
-  		},
-		
-		{
-		path: '/actividades/',
-    	url: 'actividades.html',
-    	name: 'veterinario',
-  		},
-		
-		{
-		path: '/chat/',
-    	url: 'chat.html',
-    	name: 'chat',
-  		},
+	
 		
 		
 		
